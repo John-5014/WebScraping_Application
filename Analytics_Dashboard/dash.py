47e695c2e29e9761a0a9a6995adfa8797a634e44
John Karanja Njoki <kaiserjohn52@gmail.com>
2023-07-05 13:50:56 +0300

    streamlit code for display

---
+++ Analytics_Dashboard/dash.py
@@ -0,0 +1,35 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+from streamlit_option_menu import option_menu
+from numerize.numerize import numerize
+from query import *
+
+st.set_page_config(page_title = "Analytics Dashboard", page_icon = "📊",layout="wide")
+st.subheader("📈 Descriptive Analysis")
+st.markdown("##")
+
+#fetching data
+
+result = view_all_data()
+df = pd.DataFrame(result, columns=["Policy","Expiry","Location","State","Region","Investment","Construction","BusinessType","Earthquake","Flood","Rating","id"])
+# st.dataframe(df)
+#sidebar
+st.sidebar.image("../data/images/logo.png", caption = "Online Analytics")
+st.sidebar.header("Please filter")
+
+region = st.sidebar.multiselect(
+    "Select Region",
+    options = df["Region"].unique(),
+    default = df["Region"].unique(),
+)
+location = st.sidebar.multiselect(
+    "Select Location",
+    options = df["Location"].unique(),
+    default = df["Location"].unique(),
+)
+construction = st.sidebar.multiselect(
+    "Select Construction",
+    options = df["Construction"].unique(),
+    default = df["Construction"].unique(),
+)

c5b7178846f4ffc09d313bb34d0fd24fc5052451
John Karanja Njoki <kaiserjohn52@gmail.com>
2023-07-25 06:23:25 +0300

    update main function

--- Analytics_Dashboard/app.py
+++ Analytics_Dashboard/app.py
@@ -66,9 +66,9 @@ def predict_laptop_price():
         query = np.array([company, type, ram, weight, touchscreen, ips, ppi, cpu, hdd, ssd, gpu, os])
 
         query = query.reshape(1, 12)
-        st.title("The predicted price for this laptop is " + str(int(np.exp(pipe.predict(query)[0]))))
+        st.title("The predicted price for this laptop is Ksh " + str(int(np.exp(pipe.predict(query)[0]))))
 
 # Call the function to display the page in Streamlit
-if __name__ == '__main__':
+
     
     predict_laptop_price()
